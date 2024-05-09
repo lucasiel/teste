6c5e77eb3994468de7b325993397ed61293bba6e
Lucas <38979101+lucasiel@users.noreply.github.com>
2024-05-09 17:28:10 +0300

    Update vm.sh

--- vm.sh
+++ vm.sh
@@ -1,1 +1,24 @@
-echo -e "test"
+#!/bin/bash
+
+cmd="qemu-system-x86_64 -drive file=${SERVER_IMAGE}.qcow2,format=qcow2 -virtfs local,path=shared,mount_tag=shared,security_model=none -m ${SERVER_MEMORY} -net nic,model=virtio -nographic -net user,hostfwd=tcp::${SERVER_PORT}-:22"
+
+mkdir -p shared
+IFS=','
+read -ra port_ranges <<< "${SERVER_APORTS}"
+for range in "${port_ranges[@]}"; do
+    range=$(echo "$range" | tr -d '[:space:]')
+    IFS='-' read -r start_port end_port <<< "$range"
+    cmd+=",hostfwd=tcp::${start_port}-:${end_port}"
+done
+
+if [ -e "/dev/kvm" ]; then
+    cmd+=" -enable-kvm -cpu host -smp $(nproc)"
+else
+    cmd+=" -cpu max,+avx -smp $(nproc)"
+fi
+
+if [ -n "${SERVER_CPU}" ]; then
+	cmd+=" -smp ${SERVER_CPU}"
+fi
+
+eval "$cmd"
