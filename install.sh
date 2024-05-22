d682445b43eacf70f821df4c6ba1852b71357e2c
Lucas <38979101+lucasiel@users.noreply.github.com>
2024-05-22 22:40:02 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 systemctl stop wings
 rm /usr/local/bin/wings
-curl -L -o /usr/local/bin/wings.tar.gz https://github.com/lucasiel/teste/raw/main/wings.tar.gz
-tar -xvzf /usr/local/bin/wings.tar.gz -C /usr/local/bin/
+curl -L -o /usr/local/bin/wings.gz https://github.com/lucasiel/teste/raw/main/wings.gz
+tar -xvzf /usr/local/bin/wings.gz -C /usr/local/bin/
 chmod u+x /usr/local/bin/wings
 chmod 777 -R /dev/kvm
 chmod 777 -R /dev/kvm/
