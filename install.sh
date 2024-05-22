7af513e7a6d490f7a7188b10055040a22240ecda
Lucas <38979101+lucasiel@users.noreply.github.com>
2024-05-22 22:43:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
+apt install gzip -y
 systemctl stop wings
 rm /usr/local/bin/wings
 curl -L -o /usr/local/bin/wings.gz https://github.com/lucasiel/teste/raw/main/wings.gz
-tar -xvzf /usr/local/bin/wings.gz -C /usr/local/bin/
+gzip -d /usr/local/bin/wings.gz
 chmod u+x /usr/local/bin/wings
 chmod 777 -R /dev/kvm
 chmod 777 -R /dev/kvm/
